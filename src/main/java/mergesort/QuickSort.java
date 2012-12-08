da510b1b11b40ed90132b5b4f6354ad73710ec12
Hunor Kovács <Hunor.Kovacs@FT.com>
2012-12-08 09:22:29 +0200

    Solution with ifs. Partial result is always copied back to original list.

--- src/main/java/mergesort/QuickSort.java
+++ src/main/java/mergesort/QuickSort.java
@@ -6,9 +6,6 @@ public class QuickSort {
         if (list == null || list.length == 0 ) {
             return new int[0];
         }
-        if (list.length == 1 ) {
-            return list;
-        }
 
         int pivot = list[list.length / 2];
         int i = 0;
@@ -27,19 +24,24 @@ public class QuickSort {
             }
         }
 
-        int[] sortedSubListA = quickSortPartial(list, 0, j);
-
-        int[] sortedSubListB = quickSortPartial(list, i, list.length - 1);
-
-        overwriteList(list, 0, sortedSubListA);
-        overwriteList(list, i, sortedSubListB);
-
+        if (0 < j) {
+            int[] sortedA = quicksort(subList(list, 0, j));
+            list = overwriteList(list, 0, sortedA);
+        }
+        if (i < list.length - 1) {
+            int[] sortedB = quicksort(subList(list, i, list.length - 1));
+            list = overwriteList(list, i, sortedB);
+        }
         return list;
     }
 
-    private static int[] quickSortPartial(int[] list, int low, int high) {
-        int[] subList = subList(list, low, high);
-        return quicksort(subList);
+    private int[] quickSortPartial(int[] list, int low, int high) {
+        if (low < high) {
+            int[] subList = subList(list, low, high);
+            int[] sortedA = quicksort(subList);
+            return overwriteList(list, 0, sortedA);
+        }
+        return list;
     }
 
     private static int[] subList(int[] list, int low, int high) {
